d3ba451f21cb903a6b258bbffd7ad133e8e713b0
VasilyDrozhzhin <vasilydrozhzhin@gmail.com>
2024-10-21 23:44:00 +0300

    Add files via upload

--- Module_3_1.py
+++ Module_3_1.py
@@ -1,3 +1,4 @@
+# https://github.com/VasilyDrozhzhin/Lesson0/blob/main/Module_3_1.py
 calls = 0
 def count_calls():
 	global calls
@@ -6,12 +7,15 @@ def string_info(s):
 	l = (len(s), s.upper(), s.lower())
 	count_calls()
 	return  l
-def is_contain(s = '', l = []):
+def is_contain(s, l):
 	if s in l:
 		x = True
+	else:
+		x = False
 	count_calls()
 	return x
 print(string_info('urban'))
 print(string_info('hello'))
 print(is_contain('o', 'hello'))
+print(is_contain('no', 'hello'))
 print(calls)
--- Module_3_2.py
+++ Module_3_2.py
@@ -1,5 +1,6 @@
-def send_email(message, recipient, sender = "university.help@gmail.com"):
-	if '@' not in recipient or '@' not in sender or all(element not in recipient for element in ['.ru', '.com', '.net']) or all(element not in sender for element in ['.ru', '.com', '.net']):
+# https://github.com/VasilyDrozhzhin/Lesson0/blob/main/Module_3_2.py
+def send_email(message, recipient, *, sender = "university.help@gmail.com"):
+	if '@' not in recipient or '@' not in sender or all(recipient.endswith(suffix) == 0 for suffix in ['.ru', '.net', '.com']) or all(sender.endswith(suffix) == 0 for suffix in ['.ru', '.net', '.com']):
 		print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 	elif recipient == sender:
 		print('Нельзя отправить письмо самому себе')
@@ -10,4 +11,4 @@ def send_email(message, recipient, sender = "university.help@gmail.com"):
 send_email('Тест', 'info@ya.ru')
 send_email('Hello', 'test.com')
 send_email('New message', 'university.help@gmail.com')
-send_email('Text', '1@go.ru', 'ya@mail.com')
+send_email('Text', '1@go.ru', sender= 'ya@mail.com')
